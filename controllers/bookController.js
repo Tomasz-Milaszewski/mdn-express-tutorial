30a5753eadea055a0a34e7fee8ab22d88aa0a156
Tomasz Miłaszewski <tmilaszewski@getresponse.com>
2021-04-25 20:21:02 +0200

    Add book list view feeded with data

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -28,9 +28,17 @@ exports.index = function(req, res) {
     });
 };
 
-// Display list of all books.
-exports.book_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: Book list');
+// Display list of all Books.
+exports.book_list = function(req, res, next) {
+
+    Book.find({}, 'title author') // No filter means all books ; projection means only title and author to be returned
+        .populate('author') // this call replaces id with the actual author's data as we need it
+        .exec(function (err, list_books) {
+            if (err) { return next(err); }
+            //Successful, so render
+            res.render('book_list', { title: 'Book List', book_list: list_books });
+        });
+
 };
 
 // Display detail page for a specific book.
